190c1433c131969464e423448a5fc532b4a4c64d
Morgan Meliment <morganmeliment@gmail.com>
2016-12-10 18:40:59 -0500

    Updated from Brython Server: 12/10/2016 6:41:32 PM

--- ggame/ggame.py
+++ ggame/ggame.py
@@ -1716,7 +1716,7 @@ class App(object):
             for sprite2 in App._solidSprites:
                 if sprite2 not in coll_dict:
                     coll_dict[sprite2] = []
-                if (not sprite2 in coll_dict[sprite]) and sprite != sprite2:
+                if (not (sprite2 in coll_dict[sprite])) and sprite != sprite2:
                     if not (sprite.xmin > sprite2.xmax or sprite.xmax < sprite2.xmin or sprite.ymin > sprite2.ymax or sprite.ymax < sprite2.ymin):
                         coll_dict[sprite].append(sprite2)
                         coll_dict[sprite2].append(sprite)
