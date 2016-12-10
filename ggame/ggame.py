167ecf3d4218494d7172b4736fac2ed4339fd802
Morgan Meliment <morganmeliment@gmail.com>
2016-12-10 16:58:58 -0500

    Updated from Brython Server: 12/10/2016 4:59:32 PM

--- ggame/ggame.py
+++ ggame/ggame.py
@@ -1316,11 +1316,13 @@ class SolidSprite(object):
     def colliding(self, sprites):
         self.spriteCollisions = sprites
         self.classCollisions = {}
+        for y in App.spritesdict:
+            self.classCollisions[type(y)] = []
         for x in sprites:
-            if not self.classCollisions[type(x)]:
-                self.classCollisions[type(x)] = [x]
-            else:
-                self.classCollisions[type(x)].append(x)
+            #if not self.classCollisions[type(x)]:
+            #    self.classCollisions[type(x)] = [x]
+            #else:
+            self.classCollisions[type(x)].append(x)
 
     def destroy(self):
         """
